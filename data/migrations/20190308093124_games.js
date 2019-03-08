255960b618aeb3edc835352bd683e90f67300790
Alvin <huerox@msn.com>
2019-03-08 10:04:13 -0800

    ran migrations and tests with all tests passing.

--- data/migrations/20190308093124_games.js
+++ data/migrations/20190308093124_games.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable("games", table => {
+        table.increments();
+        table.string("title", 256).notNullable();
+        table.string("genre", 256).notNullable();
+        table.integer("releaseYear");
+    })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists("games")
 };
